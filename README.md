f0166f4aaf95db68e45d8776c7a32a155d3b4276
Keith Ratliff <jkeithr01@yahoo.com>
2022-02-23 09:46:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# vanderpol
+Van der Pol Oscillator simulator
+
+A program that generates time series data for a Van der Pol ocsillator.
---
+++ vanderpol.py
@@ -0,0 +1,14 @@
+# vanderpol.py
+# A program to generate the output of a Van der Pol oscillator
+# INPUT: model parameters gathered from command prompt input
+#   note: a future version can have a GUI, like a web form
+# OUTPUT: a 2-d array of data points, [time(i), output(i)]
+#   output will be written to CSV at first, 
+#   but can later be diplayed in the GUI (web form response)
+
+# gather input parameters
+
+# run model and generate output array
+
+# write output
+
